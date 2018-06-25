053ac271b28d6d9aa182b80a375ec6c8717b192e
joshuacoyne <josh.j.coyne@gmail.com>
2018-06-25 17:28:05 -0400

    Bookmarks app created. Model created

--- djorg/settings.py
+++ djorg/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
+    'bookmarks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
