71cded7df4cab08ebd9c063c3d65dbbfee411cf7
joshuacoyne <josh.j.coyne@gmail.com>
2018-06-26 18:05:29 -0400

    Added PersonalBookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Bookmark(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -7,3 +8,6 @@ class Bookmark(models.Model):
     url = models.URLField(max_length=200)
     creadted_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBookmark(Bookmark):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
@@ -9,4 +10,7 @@ class Note(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    #TODO: Taggin system or categories
+    category = models.CharField(max_length=20)
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
